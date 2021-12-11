41a1e2588fbcd89ba328e293cc940c27e673d581
Alazar-dev <mealazartadesse@gmail.com>
2021-12-11 13:04:55 +0300

    fix: added and handled different routes

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 import Routes from "./src/routes";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Routes />
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,30 @@
 import React from "react";
 import Home from "../screens/Home";
-import { NavigationContainer } from "@react-navigation/native";
-
 import Signin from "../screens/auth/Signin";
 import Signup from "../screens/auth/Signup";
-
 import Breakfast from "../screens/Breakfast";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SuggestedFoods from "../screens/Suggested Foods";
+import ChangeFood from "../screens/Change Food";
 import CreateFood from "../screens/CreateFood";
+import Profile from "../screens/Profile";
+import Dinner from "../screens/Dinner";
+import Lunch from "../screens/Lunch";
 
 export default function Routes() {
   const Stack = createNativeStackNavigator();
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Signin" component={Signin} />
-          <Stack.Screen name="Signup" component={Signup} />
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Break Fast" component={Breakfast} />
-          <Stack.Screen name="Suggested Foods" component={SuggestedFoods} />
-          <Stack.Screen name="Create Food" component={CreateFood} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signin" component={Signin} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Breakfast" component={Breakfast} />
+        <Stack.Screen name="Dinner" component={Dinner} />
+        <Stack.Screen name="Lunch" component={Lunch} />
+        <Stack.Screen name="Change Food" component={ChangeFood} />
+        <Stack.Screen name="Create Food" component={CreateFood} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
     </>
   );
 }
